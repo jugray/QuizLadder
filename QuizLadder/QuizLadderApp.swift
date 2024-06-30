dc058ff50ac227d5faaf6f9450ae7c40a0443aa7
Justin Gray <jugray@gmail.com>
2024-06-30 12:05:49 -0500

    Initial Commit

---
+++ QuizLadder/QuizLadderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizLadderApp.swift
+//  QuizLadder
+//
+//  Created by Justin Gray on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizLadderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

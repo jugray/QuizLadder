296d1fb1d8347d6e83b6f929867a8bd0ffdddce5
Justin Gray <jugray@gmail.com>
2024-07-03 12:47:49 -0500

    Started end game conditions and views.

---
+++ QuizLadder/Views/GameOverView.swift
@@ -0,0 +1,18 @@
+//
+//  GameOver.swift
+//  QuizLadder
+//
+//  Created by Justin Gray on 7/3/24.
+//
+
+import SwiftUI
+
+struct GameOverView: View {
+    var body: some View {
+        Text("Game OVER!")
+    }
+}
+
+#Preview {
+    GameOverView()
+}

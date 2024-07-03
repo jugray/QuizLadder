8a3f1d9f88746d45f760659401301d6d413e2359
Justin Gray <jugray@gmail.com>
2024-07-03 18:45:23 -0500

    UI Tinkering

--- QuizLadder/Views/CompletedQuizQuestionView.swift
+++ QuizLadder/Views/CompletedQuizQuestionView.swift
@@ -9,23 +9,31 @@ import SwiftUI
 
 struct CompletedQuizQuestionView: View {
     
+    let backgroundGradient = LinearGradient(
+        colors: [Color.yellow, Color.orange],
+        startPoint: .topTrailing   , endPoint: .bottomLeading)
+    
     let questionText : String
     let questionAnswer : String
     
     var body: some View {
-
-        VStack{
-            Text(questionText)
-        }
-            .padding()
-        VStack{
-            Text(questionAnswer)
-                .frame(maxWidth: .infinity, alignment: .leading)
-
-        }
-        .padding()
+        
+        ZStack{
+            backgroundGradient
+                .ignoresSafeArea()  
+            VStack{
+                VStack{
+                    Text(questionText)
+                }
+                .padding()
+                VStack{
+                    Text(questionAnswer)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                }
+                .padding()
+            }}
     }
-    
 }
 
 #Preview {
